e9373708e99efa4d5281e6e284df040364916474
JacksonBey <jacksongrowson@gmail.com>
2021-11-22 14:06:39 -0800

    added api call for test

--- index.js
+++ index.js
@@ -1,18 +1,78 @@
 const express = require('express');
 const {readFile, readFileSync} = require('fs').promises;
+// const fetch =import('node-fetch');
+const fetch = require('node-fetch');
+const redis = require('redis');
+
+
+
+
+
 
 const app = express();
 
-app.get('/', async (req, response) => {
-  // readFile('./home.html', 'utf8', (err, html) => {
-  //   if (err) {
-  //     response.status(500).send('Sorry out of order');
-  //   }
-  //   response.send(html);
-  response.send(await readFile('./home.html', 'utf8'));
-});
+const PORT = process.env.PORT || 3000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
-app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
+const client = redis.createClient(REDIS_PORT);
+
+// set response
+const setData = (pokeName, data) => {
+  return `<div>
+            <h1>${pokeName}</h1>
+            <img src='${data.sprites?.front_default}' ></img>
+          </div>`
+}
+
+// this function gets a random pokemon's name from pokeapi
+const getPokemonName = async (req, res, next) => {
+    // const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+    // const data = await response.json();
+    // const randomPokemon = data.results[Math.floor(Math.random() * data.results.length)];
+    // return randomPokemon.name;
+    try{
+
+      const {pokeName} = req.params;
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+      const data = await response.json();
+      
+      //set data to redis
+      client.setex(pokeName,3600, JSON.stringify(data));
+
+      res.send(setData(pokeName, data));
+
+    } catch(err){
+        console.error(err);
+        res.status(500)
+    }
+};
+
+
+app.get("/pokemon/:pokeName", getPokemonName);
+
+
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+})
+
+
+
+
+
+
+
+
+// app.get('/', async (req, response) => {
+//   // readFile('./home.html', 'utf8', (err, html) => {
+//   //   if (err) {
+//   //     response.status(500).send('Sorry out of order');
+//   //   }
+//   //   response.send(html);
+//   response.send(await readFile('./home.html', 'utf8'));
+// });
+
+// app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'));
 
 
 
--- my-module.js
+++ my-module.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
-  science: 'science'
+  science: 'science',
 }
